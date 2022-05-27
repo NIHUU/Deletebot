c4958c06562f835dc88763f34cd105af9f22bd29
BLACK-DEVILBOY <106376927+BLACK-DEVILBOY@users.noreply.github.com>
2022-05-27 15:13:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/BLACK-DEVILBOY/BLACKCATROBOT /BLACKCATROBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /BLACKCATROBOT
 fi
-cd /Ajax
+cd /BLACKCATROBOT
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting thomas shelby....🔥"
 python3 bot.py
