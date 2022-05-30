6063cd431c3cb07f5da9604522cc067e3c5fb451
BLACK-DEVILBOY <106376927+BLACK-DEVILBOY@users.noreply.github.com>
2022-05-30 09:24:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,7 +95,7 @@ PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 CH_LINK = environ.get('CH_LINK', "")
 CH_FILTER = int(environ.get('CH_FILTER', 0))
-START_IMAGE_URL = int(environ.get('START_IMAGE_URL', "")
+START_IMAGE_URL = environ.get('START_IMAGE_URL', "")
                       
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
