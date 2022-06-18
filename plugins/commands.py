4bd668dfb18564f4669ed19264dcd204d01a6f83
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-06-18 14:02:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -69,6 +69,7 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+        
     m = datetime.datetime.now()
 
     Time = m.hour
