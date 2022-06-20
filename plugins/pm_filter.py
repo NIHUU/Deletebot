ad854f4037e30be4aa2ad4afcda65e0cd67b23ff
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-06-20 08:05:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1026,7 +1026,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.OWNER_TXT2,
+            text=script.FILTER_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
         )
