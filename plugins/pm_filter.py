b5238be00b02648c321694ea5a3825d70c07ca20
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-06-24 08:19:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1045,7 +1045,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data =="carbon"
+    elif query.data == "carbon":
         buttons = [[
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help'),
             InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close_data')
