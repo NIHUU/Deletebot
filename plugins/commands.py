f60bf08b800aa493d72e4daeb52191c303e26ee7
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-07-02 09:57:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,7 +35,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            text=START_TXT,
+            text=script.START_TXT.format(message.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
         )
