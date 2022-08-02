c7fc18dafaa7e4bbaaa50da433ec18e56bdaaabe
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-08-02 15:44:06 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -91,16 +91,53 @@ async def aboutme(client, message):
 
 @Client.on_message(filters.group & filters.command('settings'))
 async def test_command(client, message):
-    buttons = [[
-        InlineKeyboardButton("Oᴘᴇɴ Hᴇʀᴇ", callback_data="settings"),
-        InlineKeyboardButton("Oᴘᴇɴ Iɴ Bᴏᴛ Pᴍ", url=f'https://t.me/{temp.U_NAME}?start=settings')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_text(
-        text=f"<b>/Wʜᴇʀᴇ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴏᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴍᴇɴᴜ ❓</b>",
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=message.message_id
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
+    chat_type = message.chat.type
+
+    if chat_type == "private":
+        grpid = await active_connection(str(userid))
+        if grpid is not None:
+            grp_id = grpid
+            try:
+                chat = await client.get_chat(grpid)
+                title = chat.title
+            except:
+                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                return
+        else:
+            await message.reply_text("I'm not connected to any groups!", quote=True)
+            return
+
+    elif chat_type in ["group", "supergroup"]:
+        grp_id = message.chat.id
+        title = message.chat.title
+
+    else:
+        return
+
+    st = await client.get_chat_member(grp_id, userid)
+    if (
+            st.status != "administrator"
+            and st.status != "creator"
+            and str(userid) not in ADMINS
+    ):
+        return
+
+    settings = await get_settings(grp_id)
+
+    if settings is not None:
+        buttons = [[
+            InlineKeyboardButton("Oᴘᴇɴ Hᴇʀᴇ", callback_data="settings"),
+            InlineKeyboardButton("Oᴘᴇɴ Iɴ Bᴏᴛ Pᴍ", url=f'https://t.me/{temp.U_NAME}?start=settings')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=f"<b>Wʜᴇʀᴇ Dᴏ Yᴏᴜ Wᴀɴᴛ Tᴏ Oᴘᴇɴ Tʜᴇ Sᴇᴛᴛɪɴɢs Mᴇɴᴜ ❓</b>",
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=message.message_id
         )
 
     
